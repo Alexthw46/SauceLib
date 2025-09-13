c85472f8d88b3ec2a5d7cf4639f678618f1fde95
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2025-09-13 17:35:10 +0200

    multipos support for PSCMEI

--- src/main/java/com/alexthw/sauce/util/PosCarryMEI.java
+++ src/main/java/com/alexthw/sauce/util/PosCarryMEI.java
@@ -6,17 +6,28 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PosCarryMEI extends MobEffectInstance {
 
     public BlockPos getOrigin() {
         return origin;
     }
 
+    public List<BlockPos> getAllOrigins() {
+        return origins;
+    }
+
+    @Deprecated
     BlockPos origin;
+
+    List<BlockPos> origins = new ArrayList<>();
 
     public PosCarryMEI(Holder<MobEffect> pEffect, int duration, int amp, boolean ambient, boolean show, BlockPos origin) {
         super(pEffect, duration, amp, ambient, show);
         this.origin = origin;
+        this.origins.add(origin);
     }
 
     //update the origin when updating the effect
@@ -25,8 +36,10 @@ public class PosCarryMEI extends MobEffectInstance {
 
         if (pOther instanceof PosCarryMEI other) {
             this.origin = other.getOrigin();
+            this.origins.addAll(other.getAllOrigins());
         }
 
         return super.update(pOther);
     }
+
 }
