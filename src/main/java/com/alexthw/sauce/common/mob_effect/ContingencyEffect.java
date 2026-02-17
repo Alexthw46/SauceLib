5c36c03f881fd7cb05b9c330edc65740abc3c8fb
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2026-02-17 12:22:19 +0100

    Fix contingencies being cleared by dispels

--- src/main/java/com/alexthw/sauce/common/mob_effect/ContingencyEffect.java
+++ src/main/java/com/alexthw/sauce/common/mob_effect/ContingencyEffect.java
@@ -2,6 +2,11 @@ package com.alexthw.sauce.common.mob_effect;
 
 import com.hollingsworth.arsnouveau.common.potions.PublicEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.neoforged.neoforge.common.EffectCure;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
 
 public class ContingencyEffect extends PublicEffect {
 
@@ -9,4 +14,7 @@ public class ContingencyEffect extends PublicEffect {
         super(MobEffectCategory.NEUTRAL, 0);
     }
 
+    @Override
+    public void fillEffectCures(@NotNull Set<EffectCure> cures, @NotNull MobEffectInstance effectInstance) {
+    }
 }
